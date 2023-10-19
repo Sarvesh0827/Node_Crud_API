0a0ac4d0f199e479a7202bab95562c9da1228da2
Sarvesh Reshimwale <92729924+Sarvesh0827@users.noreply.github.com>
2023-10-19 09:25:55 +0530

    initial project

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app =express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello Node API')
+})
+
+app.listen(3000,()=> {
+    console.log('Node Api running on port 3000');
+    
+})
